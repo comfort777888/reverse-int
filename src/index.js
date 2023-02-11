376128066c075fae72863b78e542e3d531c4282a
Araya <aray2891@mail.ru>
2023-02-11 20:41:30 +0600

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function reverse (n) {
+    if (n<0) {
+        n=(-n)
+    }
+
+    let i=0;
+    let res="";
+    let nString=n.toString();
+    for (let i=nString.length-1; i>=0; i--) {
+        res=`${res}${nString[i]}`  
+    }
+    
+
+    while (i<res.length) {     
+        if (res[i]!=="0") {
+            break
+        } else {
+            res=res.slice(i+1)
+           
+        }
+        i++
+    
+    }
+return res
   
 }
+
+//console.log(reverse(584232000))
